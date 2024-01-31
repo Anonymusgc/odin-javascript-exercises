2ad361afd523d7f3b843277324b9afcdc2341bec
Anonymusgc <120783414+Anonymusgc@users.noreply.github.com>
2024-02-01 00:18:08 +0100

    Do 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const getAge = (birth, death) => {
+  if (!death) {
+    {
+      death = new Date().getFullYear();
+    }
+  }
+  return death - birth;
+};
 
+const findTheOldest = function (people) {
+  return people.reduce((before, person) => {
+    const age = getAge(person.yearOfBirth, person.yearOfDeath);
+    const beforeAge = getAge(before.yearOfBirth, before.yearOfDeath);
+    console.log(age);
+    console.log(beforeAge);
+
+    if (age > beforeAge) {
+      // console.log(person);
+      return person;
+    }
+    return age > before ? person : before;
+  });
 };
 
 // Do not edit below this line
