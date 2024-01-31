d219610420417afabedd10c631b7858c1de4eea9
Anonymusgc <120783414+Anonymusgc@users.noreply.github.com>
2024-01-31 23:38:16 +0100

    Do 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+  let foreward = words.replaceAll("!", "");
+  foreward = foreward.replaceAll(".", "");
+  foreward = foreward.replaceAll(",", "");
+  foreward = foreward.toLowerCase();
+  forewardArr = foreward.split(" ");
+  foreward = forewardArr.join("");
+  // console.log(foreward);
+  let backwardArr = [];
+  for (let i = foreward.length - 1; i >= 0; i--) {
+    backwardArr.push(foreward[i]);
+  }
+  const backward = backwardArr.join("");
+  if (foreward == backward) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
