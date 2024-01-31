9755a180e9b9726c08b688b0079b7c626a340492
Anonymusgc <120783414+Anonymusgc@users.noreply.github.com>
2024-01-31 23:47:49 +0100

    Do 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (seqNumber) {
+  seqNumber = Number(seqNumber);
+  if (seqNumber == 0) {
+    return 0;
+  }
+  if (seqNumber < 0) {
+    return "OOPS";
+  }
+  const fibonacciArray = [1, 1];
+  for (let i = 2; i < seqNumber; i++) {
+    fibonacciArray.push(fibonacciArray[i - 2] + fibonacciArray[i - 1]);
+  }
+  // console.log(fibonacciArray);
+  return fibonacciArray[seqNumber - 1];
 };
 
 // Do not edit below this line
